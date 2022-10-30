3d6594ece6f67204e72c372a33b19cbb3379b1df
mzyrkowski <marcinzyrkowski7@gmail.com>
2022-10-30 12:29:53 +0100

    added testing framework

---
+++ src/main/java/com/library/controllers/HomePageController.java
@@ -0,0 +1,19 @@
+package com.library.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomePageController {
+
+    private static final String GREETING_MESSAGE = "hello world from library app";
+
+    @RequestMapping(value = "/library", method = RequestMethod.GET)
+    public ResponseEntity<String> hello() {
+        return new ResponseEntity<>(GREETING_MESSAGE, HttpStatus.OK);
+    }
+
+}
---
+++ src/test/java/com/library/FirstTest.java
@@ -0,0 +1,13 @@
+package com.library;
+
+import org.junit.Test;
+
+public class FirstTest {
+
+    @Test
+    public void test() {
+        System.out.println("passed");
+    }
+
+
+}
