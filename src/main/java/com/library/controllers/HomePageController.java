34aa8c909ba280c923b31a047f9d948aba315b91
mzyrkowski <marcinzyrkowski7@gmail.com>
2022-10-30 19:23:27 +0100

    home page controller enhancement

--- src/main/java/com/library/controllers/HomePageController.java
+++ src/main/java/com/library/controllers/HomePageController.java
@@ -3,15 +3,14 @@ package com.library.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomePageController {
 
     private static final String GREETING_MESSAGE = "hello world from library app";
 
-    @RequestMapping(value = "/library", method = RequestMethod.GET)
+    @GetMapping(value = {"/"})
     public ResponseEntity<String> getHomePage() {
         return new ResponseEntity<>(GREETING_MESSAGE, HttpStatus.OK);
     }
